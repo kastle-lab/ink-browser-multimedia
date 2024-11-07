683dbbd31ff8950fe99a8cf7dd94818d6704374e
jonispatented <rockerjon01@gmail.com>
2024-11-06 23:44:04 -0500

    Fixed the video component's sizing issue. It now fits itself into the remaining space in its quadrant. Added a text field to the top of the quadrant that allows for changing the video currently playing.

--- browser/src/components/Video.js
+++ browser/src/components/Video.js
@@ -1,14 +1,17 @@
 
 import React, { useState } from 'react'
 import YouTube from "react-youtube";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
 
 function Video({ setItemInVideoData }) {
 
     const [videoId, setVideoId] = useState("y7sXDpffzQQ");
+    const [videoSearch, setVideoSearch] = useState("");
 
     const opts = {
-        height: '390',
-        width: '640',
+        width: '100%',
+        height: '100%',
         playerVars: {
             // https://developers.google.com/youtube/player_parameters
             autoplay: 1,
@@ -16,8 +19,20 @@ function Video({ setItemInVideoData }) {
     }
 
     return (
-        <div>
+        <div style={{width: '100%', height: '100%', objectFit: 'cover', display: 'flex', flexDirection: 'column' }}>
+            <div className='right-search' style={{ width: '100%', paddingBottom: '5px' }}>
+                <TextField
+                    style={{ width: '100%' }}
+                    size="small"
+                    id="video-id-field"
+                    label="Video ID"
+                    value={videoSearch}
+                    onChange={(e) => setVideoSearch(e.target.value)}
+                />
+                <Button onClick={() => setVideoId(videoSearch)} variant="contained">Enter</Button>
+            </div>
             <YouTube
+                style={{width: '100%', height: '100%'}}
                 videoId={videoId}
                 opts={opts}
                 onPause={(_) => {
